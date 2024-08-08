32c35b6737936c5cf8a34cbe0af242976860d84e
Eko Manurung <eko.manurung@grabtaxi.com>
2024-08-08 16:55:10 +0700

    [improvement-2] add endpoint listAll

--- src/main/java/com/inventory/controller/InventoryRestController.java
+++ src/main/java/com/inventory/controller/InventoryRestController.java
@@ -3,29 +3,35 @@ package com.inventory.controller;
 import com.inventory.dao.InventoryRepository;
 import com.inventory.model.Inventory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by eko.j.manurung on 9/13/2016.
  */
-@RequestMapping(value = "/api/v1/inventory")
+@RequestMapping(value = "/api/v1/inventories")
 @RestController
 public class InventoryRestController {
 
     @Autowired
     private InventoryRepository repository;
 
-    @GetMapping
-    public ResponseEntity<Inventory> getInventory(@RequestParam(value = "id", defaultValue = "1") String id) {
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Inventory> getInventory(@PathVariable("id") String id) {
         repository
                 .findById(id)
                 .ifPresent(inventory -> new ResponseEntity<>(inventory, HttpStatus.OK));
 
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Inventory>> findAll(@RequestParam int page, @RequestParam int size) {
+        return new ResponseEntity<>(repository.findAll(PageRequest.of(page, size)).getContent(), HttpStatus.OK);
     }
 }
